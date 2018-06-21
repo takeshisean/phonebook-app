4f484ac8fb46419652b8a319f3ea9b590bfc0207
yapkokchin <yapkokchin@airasia.com>
2018-06-21 18:05:45 +0800

    added angular material & models

---
+++ phonebook-project/src/models/phonebook.model.ts
@@ -0,0 +1,15 @@
+export interface PhoneBookDT {
+    _id: string;
+    lastName: string;
+    firstName: string;
+    number: string;
+    phoneType: PhoneTypeDT;
+    createdAt: Date;
+    __v: number;
+}
+
+export interface PhoneTypeDT {
+    _id: string;
+    type: string;
+    __v: number;
+}
