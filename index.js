f81f3d033caec22052894b47f38884d20d379c49
yapkokchin <yapkokchin@airasia.com>
2018-06-21 17:38:45 +0800

    added dotenv & mongoose

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
+const db = require('./db');
 
-var port = 3000;
+require('dotenv').config();
+global.__root = __dirname + '/';
+
+var port = process.env.PORT || 3000;
+
+//Setting up CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', (req, res) => {
     res.status(200).send('My phone book app.');
